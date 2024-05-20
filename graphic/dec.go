4cab35ad9349860317629b882a3370d4e92759c6
William Zeni <williamzeni56@gmail.com>
2024-05-20 19:00:43 +0200

    starting thinking about dir encryption

--- graphic/dec.go
+++ graphic/dec.go
@@ -3,6 +3,7 @@ package graphic
 import (
 	"fmt"
 	"ghoji/encryptor"
+	"os"
 	"sync"
 	"time"
 )
@@ -17,36 +18,47 @@ func DoDecryption(path string, numCpu int, goroutines int) {
 		goroutines = encryptor.DefaultGoRoutines
 	}
 
+	info, err := os.Stat(path)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 	var passwd string
 
 	fmt.Print("Insert password: ")
-	_, err := fmt.Scanf("%s", &passwd)
+	_, err = fmt.Scanf("%s", &passwd)
 	if err != nil {
 		panic(err)
 	}
 
-	progress := make(chan float64)
-	var wg sync.WaitGroup
-
-	wg.Add(1)
-	go func() {
-		for p := range progress {
-			fmt.Print("\r")
-			fmt.Printf("Progress: %d %%", int(p*100))
-		}
-		wg.Done()
-	}()
-
-	fmt.Printf("Decrypting file: %s \nwith %d CPUs and %d goroutines\n", path, numCpu, goroutines)
 	startTime := time.Now()
+	if info.IsDir() {
+		//todo
+	} else {
+		progress := make(chan float64)
+		var wg sync.WaitGroup
 
-	err = encryptor.DecryptFile(passwd, path, numCpu, goroutines, progress)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
+		wg.Add(1)
+		go func() {
+			for p := range progress {
+				fmt.Print("\r")
+				fmt.Printf("Progress: %d %%", int(p*100))
+			}
+			wg.Done()
+		}()
+
+		fmt.Printf("Decrypting file: %s \nwith %d CPUs and %d goroutines\n", path, numCpu, goroutines)
+
+		err = encryptor.DecryptFile(passwd, path, numCpu, goroutines, progress)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		wg.Wait()
+
 	}
-
-	wg.Wait()
 
 	elapsedTime := time.Since(startTime)
 	fmt.Println("\n\nElapsed time:", elapsedTime)
